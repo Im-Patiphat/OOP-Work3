2efe6086c61657f9148a2d3b4142c63bfbe84e75
Patiphat <123315700+Im-Patiphat@users.noreply.github.com>
2024-02-25 23:23:14 +0700

    Add files via upload

--- Author.js
+++ Author.js
@@ -1,104 +1,104 @@
 class Author {
-    name = "";
-    email = "";
-    gender = '';
-    constructor(name ,email ,gender ){
-        this.name = name;
-        this.email = email 
-        this.gender = gender
-    }
-    
-    getName(){
-        return this.name
-    }
-    getEmail(){
-        return this.email
-    }
-    setEmail(email){
-        this.email = email;
-    }
-    toString(){
-        return "Author [ name = "+this.name+", Email = "+this.email+", Gender = "+this.gender+" ]";
-    }
+  name = "";
+  email = "";
+  gender = '';
+  constructor(name ,email ,gender ){
+      this.name = name;
+      this.email = email 
+      this.gender = gender
+  }
+  
+  getName(){
+      return this.name
+  }
+  getEmail(){
+      return this.email
+  }
+  setEmail(email){
+      this.email = email;
+  }
+  toString(){
+      return "Author [ name = "+this.name+", Email = "+this.email+", Gender = "+this.gender+" ]";
+  }
 }
 class Book {
-    name = "";
-    authors = [];
-    price = 0;
-    qty = 0 ;
-    constructor(name , authors,price ,qty = 0 ){
-        this.name = name;
-        this.authors = authors;
-        this.price =price;
-        this.qty = qty;
-    }
-    // constructor(name , price ,qty){
-    //     this.name = name;
-    //     this.author = author;
-    //     this.price =price;
-    //     this.qty = qty;
-    // }
-    getName() {
-        return this.name;
-    }
+  name = "";
+  authors = [];
+  price = 0;
+  qty = 0 ;
+  constructor(name , authors,price ,qty = 0 ){
+      this.name = name;
+      this.authors = authors;
+      this.price =price;
+      this.qty = qty;
+  }
+  // constructor(name , price ,qty){
+  //     this.name = name;
+  //     this.author = author;
+  //     this.price =price;
+  //     this.qty = qty;
+  // }
+  getName() {
+      return this.name;
+  }
 
-    getAuthor() {
-        return this.author;
-    }
+  getAuthor() {
+      return this.author;
+  }
 
-    getPrice() {
-        return this.price;
-    }
+  getPrice() {
+      return this.price;
+  }
 
-    setPrice(price) {
-        this.price = price;
-    }
+  setPrice(price) {
+      this.price = price;
+  }
 
-    getQty() {
-        return this.qty;
-    }
+  getQty() {
+      return this.qty;
+  }
 
-    setQty(qty) {
-        this.qty = qty;
-    }
+  setQty(qty) {
+      this.qty = qty;
+  }
 
-    toString() {
-        let BookDetail = "";
-        for (let i = 0; i < this.authors.length; i++) {
-            BookDetail += this.authors[i].toString();
-            if (i < this.authors.length -1 ) {
-                BookDetail += ', ';
-            }
-        }
-        return `Book [name = ${this.name}, authors = {${BookDetail}}, price = ${this.price}, qty = ${this.qty}]`;
-    }
+  toString() {
+      let BookDetail = "";
+      for (let i = 0; i < this.authors.length; i++) {
+          BookDetail += this.authors[i].toString();
+          if (i < this.authors.length -1 ) {
+              BookDetail += ', ';
+          }
+      }
+      return `Book [name = ${this.name}, authors = {${BookDetail}}, price = ${this.price}, qty = ${this.qty}]`;
+  }
 
-    getAuthorName() {
-        let AuthorName = "";
-       for (let i = 0; i < this.authors.length; i++) {
-        AuthorName += this.authors[i].getName();
-        if (i < this.authors.length -1 ) {
-            AuthorName += ', ';
-        }
-       }
-       return `"${AuthorName}"`
-    }
+  getAuthorName() {
+      let AuthorName = "";
+     for (let i = 0; i < this.authors.length; i++) {
+      AuthorName += this.authors[i].getName();
+      if (i < this.authors.length -1 ) {
+          AuthorName += ', ';
+      }
+     }
+     return `"${AuthorName}"`
+  }
 
 }
- const main = () => {
-    const author1 = new Author ("Oat" , "Oat@gmail.com" ,"M");
-    const author2 = new Author ("Aom" , "Aom@gmail.com" ,"F");
+const main = () => {
+  const author1 = new Author ("Oat" , "Oat@gmail.com" ,"M");
+  const author2 = new Author ("Aom" , "Aom@gmail.com" ,"F");
 
-    const book1 = new Book ("นิยาย" ,[author1,author2] ,599,7);
-    const book2 = new Book ("การ์ตูน" ,[author2] ,399,1);
+  const book1 = new Book ("นิยาย" ,[author1,author2] ,599,7);
+  const book2 = new Book ("การ์ตูน" ,[author2] ,399,1);
 
-    
+  
 
-    // console.log(author1.toString());
-    // console.log(author2.toString());
-    console.log(book1.toString());
-    console.log(book2.toString());
+  // console.log(author1.toString());
+  // console.log(author2.toString());
+  console.log(book1.toString());
+  console.log(book2.toString());
 
-    console.log(book1.getAuthorName());
- }
- main()
+  console.log(book1.getAuthorName());
+}
+main()
--- Customer.js
+++ Customer.js
@@ -1,80 +1,77 @@
-class Customer{
-    id = null;
-    name = "";
-    discount = 0;
-    constructor(id,name,discount){
-        this.id = id;
-        this.name = name;
-        this.discount = discount;
-    }
-    getId() {
-        return this.id;
-      }
-    
-      getName() {
-        return this.name;
-      }
-    
-      getDiscount() {
-        return this.discount;
-      }
-    
-      setDiscount(discount) {
-        this.discount = discount;
-      }
-    
-      toString() {
-        return `${this.name}(${this.id}) (${this.discount}%)`;
-      }
-    }
 
-    class Invoice{
-        id = 0;
-        customer = null;
-        amount = 0;
-        constructor(id, customer, amount){
-            this.id = id;
-            this.customer = customer;
-            this.amount = amount;
-        }
-        getId(){
-            return this.id;
-        }
-        getCustomer(){
-            return this.customer;
-        }
-        setCustomer(customer){
-            this.customer = customer;
-        }
-        getAmount(){
-            return this.amount;
-        }
-        setAmount(amount){
-            this.amount = amount;
-        }
-        getCustomerId(){
-            return this.customer.getId();
-        }
-        getCustomerName(){
-            return this.customer.getName();
-        }
-        getCustomerDiscount(){
-            return this.customer.getDiscount();
-        }
-        getAmountAfterDiscount(){
-            return this.amount - (this.amount * this.customer.getDiscount()) / 100;
-        }
-        toString(){
-            return `Invoice[id=${this.id}, customer=${this.getCustomerName()} (${this.getCustomerDiscount()}%), amount=${this.amount}]`;
-        }
-    }
+class Customer {
+  id = 0;
+  name = "";
+  discount = 0;
+  constructor(id, name, discount) {
+    this.id = id;
+    this.name = name;
+    this.discount = discount;
+  }
+  getId() {
+    return this.id;
+  }
+  getName() {
+    return this.name;
+  }
+  getDiscount() {
+    return this.discount;
+  }
+  setDiscount(discount) {
+    this.discount = discount;
+  }
+  toString() {
+    return ` ${this.name}(${this.id})(${this.discount}%) `;
+  }
+}
 
-    const main = ()=> {
-        const customer1 = new Customer(1,"Kay", 10);
+class Invoice {
+  id = 0;
+  customer = null;
+  amount = 0;
+  constructor(id, customer, amount) {
+    this.id = id;
+    this.customer = customer;
+    this.amount = amount;
+  }
+  getId() {
+    return this.id;
+  }
+  getCustomer() {
+    return this.customer;
+  }
+  setCustomer(customer) {
+    this.customer = customer;
+  }
+  getAmount() {
+    return this.amount;
+  }
+  setAmount(amount) {
+    this.amount = amount;
+  }
+  getCustomerId() {
+    return this.customer.getId();
+  }
+  getCustomerName() {
+    return this.customer.getName();
+  }
+  getCustomerDiscount() {
+    return this.customer.getDiscount();
+  }
+  getAmountAfterDiscount() {
+    return this.amount - (this.amount * this.customer.getDiscount()) / 100;
+  }
+  toString() {
+    return `Invoice[id=${this.id}, customer=${this.customer.toString()}, amount= ${this.amount} ]`;
+  }
+}
 
-        const invoice1 = new Invoice(1, customer1, 100)
-        console.log(customer1.toString());
-        console.log(invoice1.getAmountAfterDiscount());
-        
-    }
-    main();
+const main = () => {
+  const customer1 = new Customer(1, "Oat", 10);
+
+  const invoice1 = new Invoice(1, customer1, 100);
+  console.log(customer1.toString());
+  console.log(invoice1.toString());
+  console.log("AmountAfterDiscount = "+invoice1.getAmountAfterDiscount());
+};
+main();
