8484aba52d7e6bfc2684e9367566e1672ff4a8eb
Patiphat <123315700+Im-Patiphat@users.noreply.github.com>
2024-03-04 18:01:20 +0700

    Update Ex5.js

--- Ex5.js
+++ Ex5.js
@@ -1,102 +1,125 @@
-/* ------ Customer ------ */
 class Customer {
-  name = "";
-  member = false;
-  memberType = "";
-  constructor(name, member, memberType) {
-    this.name = name;
-    this.member = member;
-    this.memberType = memberType;
+    name = "";
+    member = false;
+    memberType = "";
+    constructor(name) {
+      this.name = name;
+    }
+    getName() {
+      return this.name;
+    }
+    isMember() {
+      return this.member;
+    }
+    setMember(member) {
+      this.member = member;
+    }
+    getMemberType() {
+      return this.memberType;
+    }
+    setMemberType(memberType) {
+      this.memberType = memberType;
+    }
+    toString() {
+      return `Customer [ name = ${this.name}, member = ${
+        this.member
+      }, memberType = ${this.getMemberType()}]`;
+    }
   }
-  getName() {
-    return this.name;
+  
+  class Visit {
+    customer = null;
+    date = "";
+    serviceExpense = 0;
+    productExpense = 0;
+    constructor(customer, date, serviceExpense, productExpense) {
+      this.customer = customer;
+      this.date = date;
+      this.serviceExpense = serviceExpense;
+      this.productExpense = productExpense;
+    }
+    getName() {
+      return this.name;
+    }
+    getServiceExpense() {
+      return this.serviceExpense;
+    }
+    setServiceExpense(serviceExpense) {
+      this.serviceExpense = serviceExpense;
+    }
+    getProductExpense() {
+      return this.productExpense;
+    }
+    setProductExpense(productExpense) {
+      this.productExpense = productExpense;
+    }
+    getTotalExpense() {
+      let total = 0;
+      let ptotal =
+        this.getProductExpense() -
+        DiscountRate.getProductDiscountRate(this.customer.getMemberType()) *
+          this.getProductExpense();
+      let stotal =
+        this.getServiceExpense() -
+        DiscountRate.getServiceDiscountRate(this.customer.getMemberType()) *
+          this.getServiceExpense();
+  
+      total = ptotal + stotal;
+      return total;
+    }
+    toString() {
+      return `Visit [ ${this.customer.toString()}, serviceExpense = ${
+        this.serviceExpense
+      } ,
+          productExpense ${
+            this.productExpense
+          }, total = ${this.getTotalExpense()}]`;
+    }
   }
-  isMember() {
-    return this.member;
+  
+  class DiscountRate {
+    static PREMIUM = new DiscountRate("premium");
+    static GOLD = new DiscountRate("gold");
+    static SILVER = new DiscountRate("silver");
+  
+    constructor(type) {
+      this.type = type;
+    }
+    static getServiceDiscountRate(type) {
+      switch (type) {
+        case "premium":
+          return 0.2;
+        case "gold":
+          return 0.15;
+        case "silver":
+          return 0.1;
+        default:
+          return 0;
+      }
+    }
+    static getProductDiscountRate(type) {
+      switch (type) {
+        case "premium":
+          return 0.1;
+        case "gold":
+          return 0.1;
+        case "silver":
+          return 0.1;
+        default:
+          return 0;
+      }
+    }
   }
-  setMember(member) {
-    this.member = member;
-  }
-  getMemberType() {
-    this.memberType;
-  }
-  setMemberType(type) {
-    this.memberType = type;
-  }
-  toString() {
-    return `Customer [ name = ${this.name}, member = ${this.member}, memberTyep = ${this.memberType}]`;
-  }
-}
-
-/* ------ Visit ------ */
-class Visit {
-  customer = null;
-  date = "";
-  serviceExpense = 0;
-  productExpense = 0;
-  constructor(customer, date) {
-    this.customer = customer;
-    this.date = date;
-    this.serviceExpense = 0;
-    this.productExpense = 0;
-  }
-  getName() {
-    return this.name;
-  }
-  getServiceExpense() {
-    return this.serviceExpense;
-  }
-  setServiceExpense(serviceExpense) {
-    this.serviceExpense = serviceExpense;
-  }
-  getProductExpense() {
-    return this.productExpense;
-  }
-  setProductExpense(productExpense) {
-    this.productExpense = productExpense;
-  }
-  getTotalExpense() {
-    return this.serviceExpense + this.productExpense;
-  }
-  toString() {
-    return `Visit [ Customer = ${this.customer}, Date = ${
-      this.date
-    }, Service Expense = ${this.serviceExpense}, Product Expense = ${
-      this.productExpense
-    }, Total = ${this.getTotalExpense()}]`;
-  }
-}
-
-/* ------ DiscountRate ------ */
-class DiscountRate {
-  static serviceDiscount = {
-    premium: 0.2,
-    gold: 0.15,
-    silver: 0.1,
+  
+  const main = () => {
+    const customer2 = new Customer("vick");
+    customer2.setMember(true);
+    customer2.setMemberType("silver");
+  
+    const visit1 = new Visit(customer2, "2024/03/04");
+    visit1.setProductExpense(150);
+    visit1.setServiceExpense(100);
+    console.log(visit1.toString());
   };
-
-  static productDiscount = {
-    premium: 0.1,
-    gold: 0.1,
-    silver: 0.1,
-  };
-  static getServiceDiscountRate(type) {
-    return this.serviceDiscount[type];
-  }
-
-  static getProductDiscountRate(type) {
-    return this.productDiscount[type];
-  }
-}
-
-/* ------ Main ------ */
-const main = () => {
-  const customer1 = new Customer("Kays", true, "001");
-  console.log(customer1.toString());
-
-  const visit1 = new Visit(customer1.name, "12/12/2022");
-  visit1.setServiceExpense(50);
-  visit1.setProductExpense(30);
-  console.log(visit1.toString());
-};
-main();
+  main();
+  
